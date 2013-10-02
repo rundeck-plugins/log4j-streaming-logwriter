2345b705d97b489b0c7339dc94eefdb973dccf5c
Alex Honor <alexhonor@yahoo.com>
2013-10-02 14:36:13 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,77 @@
+## Description
+
+This StreamingLogWriter plugin sends log messages to the specified
+log4j logger. This is useful if you want to configure an appender
+to a particular destination (eg, Syslog).
+
+# Deploy
+
+This is a groovy plugin so just copy the .groovy file to $RDECK_BASE/libext.
+
+## Configuration
+
+To enable the plugin, update the `rundeck-config.properties` file and include
+it in the "rundeck.execution.logs.streamingWriterPlugins" property.
+
+Example: rundeck-config.properties
+
+    rundeck.execution.logs.streamingWriterPlugins=Log4jStreamingLogWriterPlugin
+
+
+You can name the log4j logger anything you wish. By default, it is called "rundeck".
+To override the default, update the `project.properties` file with the following entry:
+
+    project.plugin.StreamingLogWriter.Log4jStreamingLogWriterPlugin.logger=my-logger-name
+
+
+If you are running rsyslog on Linux, be sure the rsyslog.conf enabled the
+udp/tcp input. eg:
+```
+# Provides UDP syslog reception
+$ModLoad imudp
+$UDPServerRun 514
+
+# Provides TCP syslog reception
+$ModLoad imtcp
+$InputTCPServerRun 514
+```
+
+## Usage
+
+Edit rundeck's log4j.properties to set up the appender you wish to send the log messages.
+
+Here is MDC data available for use in the log4j conversion pattern layouts.
+
+* username: User that ran the job
+* project: Project name
+* name: the job name
+* group: the job group
+* id: the job ID
+* execid: the execution ID
+* url: The URL to the execution page
+* loglevel: the log event log level
+
+Use the `%X` format specifier with one of the properties. Eg
+
+     %X{username} %X{project}:%X{group}/%X{name} %X{execid}
+
+## Example
+
+Here's an example that sends messages to syslog. The logger name is the default "rundeck"
+but change that to whatever you specified earlier.
+
+Example: log4j.properties
+
+```
+log4j.rundeck=INFO, SYSLOG
+# configure Syslog facility LOCAL1 appender
+log4j.appender.SYSLOG=org.apache.log4j.net.SyslogAppender
+log4j.appender.SYSLOG.threshold=WARN
+log4j.appender.SYSLOG.syslogHost=localhost
+log4j.appender.SYSLOG.facility=LOCAL4
+log4j.appender.SYSLOG.layout=org.apache.log4j.PatternLayout
+log4j.appender.SYSLOG.layout.ConversionPattern=%d{ISO8601} %-5p %c{1} - %X{username} %X{project} %X{group}/%X{name} #%X{execid} %X{url} - %m%n
+```
+Here's an example log message using the layout above:
+
+    Oct  2 23:18:03 2013-10-02 23: 18:03,150 INFO - rundeck admin anvils nightly_catalog_rebuild #5 http://192.168.50.2:4440/execution/follow/5 - Completed.
---
+++ src/Log4jStreamingLogWriterPlugin.groovy
@@ -0,0 +1,77 @@
+import com.dtolabs.rundeck.core.logging.LogLevel
+import com.dtolabs.rundeck.plugins.logging.StreamingLogWriterPlugin;
+
+import com.dtolabs.rundeck.core.logging.LogEvent
+import org.apache.log4j.Level
+import org.apache.log4j.Logger
+import org.apache.log4j.MDC;
+
+/**
+ * This streaming log writer sends messages to log4j
+ */
+rundeckPlugin(StreamingLogWriterPlugin) {
+
+    configuration {
+        logger = "rundeck"
+        logger required: true, description: "Logger instance name"
+
+
+    }
+    /**
+     * The "open" closure is called to open the stream for writing events.
+     * It is passed two map arguments, the execution data, and the plugin configuration data.
+     *
+     * It should return a Map containing the stream context, which will be passed back for later
+     * calls to the "addEvent" closure.
+     */
+    open { Map execution, Map config ->
+        // create the logger
+        def Logger logger = Logger.getLogger(config.logger);
+
+        // return context map for the plugin to reuse later
+        [execution: execution, config: config, logger: logger]
+    }
+
+    /**
+     * "addEvent" closure is called to append a new event to the stream.  
+     * It is passed the Map of stream context created in the "open" closure, and a LogEvent.
+     * Metadata is added to log event messages (see http://rundeck.org/docs/manual/job-workflows.html#context-variables)
+     */
+    addEvent { Map context, LogEvent event ->
+        def Level level;
+        switch (event.loglevel) {
+            case LogLevel.ERROR:
+                level = Level.ERROR
+                break
+            case LogLevel.WARN:
+                level = Level.WARN
+                break
+            case LogLevel.VERBOSE:
+            case LogLevel.DEBUG:
+                level = Level.DEBUG
+                break
+            case LogLevel.NORMAL:
+            case LogLevel.OTHER:
+            default:
+                level = Level.INFO
+        }
+        MDC.put("username", context.execution.username)
+        MDC.put("name", context.execution.name)
+        MDC.put("group", context.execution.group)
+        MDC.put("execid", context.execution.execid)
+        MDC.put("project", context.execution.project)
+        MDC.put("id", context.execution.id)
+        MDC.put("url", context.execution.url)
+        MDC.put("loglevel", context.execution.loglevel)
+
+        context.logger.log(level, event.message)
+
+    }
+
+    /**
+     * "close" closure is called to end writing to the stream.
+     */
+    close { Map context ->
+
+    }
+}
